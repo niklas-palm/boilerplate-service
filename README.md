b225da2598ccc70ba88087d099def9ab804ebcc1
Niklas Palm <niklas-palm@hotmail.com>
2023-06-20 15:47:45 +0200

    Remove tracing

--- README.md
+++ README.md
@@ -7,3 +7,17 @@ Boilerplate for getting quickly getting started with multi-account deployments (
 - OIDC Github + AWS, ARN of the IAM roles in each account for Github Actions to assume with correct trust relationship set up.
 - Create the relevant parameters (`/${environment}/API/CertificateARN`) in parameter store, and the relevant NS records in the central networking account for each hosted zone.
 - The deployment to prod is using the "prod" environment in Github, which should have at least one required reviewer. This triggers a manual approval step before deploying to prod. Set up that environment for the repo.
+
+### Dependencies
+
+- Node 16
+
+```bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+
+nvm install 16
+
+nvm use 16
+```
+
+- [AWS SAM](https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/install-sam-cli.html)
--- hello-world/app.ts
+++ hello-world/app.ts
@@ -1,10 +1,8 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
 import { Metrics, MetricUnits } from '@aws-lambda-powertools/metrics';
 import { Logger } from '@aws-lambda-powertools/logger';
-import { Tracer } from '@aws-lambda-powertools/tracer';
 const metrics = new Metrics();
 const logger = new Logger();
-const tracer = new Tracer();
 
 /**
  *
@@ -19,30 +17,16 @@ const tracer = new Tracer();
 
 export const lambdaHandler = async (event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> => {
     let response: APIGatewayProxyResult;
-    // Get facade segment created by AWS Lambda
-    const segment = tracer.getSegment();
 
-    // Create subsegment for the function & set it as active
-    const handlerSegment = segment.addNewSubsegment(`## ${process.env._HANDLER}`);
-    tracer.setSegment(handlerSegment);
-
-    // Annotate the subsegment with the cold start & serviceName
-    tracer.annotateColdStart();
-    tracer.addServiceNameAnnotation();
-
-    // Add annotation for the awsRequestId
-    tracer.putAnnotation('awsRequestId', context.awsRequestId);
     // Capture cold start metrics
     metrics.captureColdStartMetric();
-    // Create another subsegment & set it as active
-    const subsegment = handlerSegment.addNewSubsegment('### MySubSegment');
-    tracer.setSegment(subsegment);
+
     try {
         // hello world code
         response = {
             statusCode: 200,
             body: JSON.stringify({
-                message: 'hello world this is a feature',
+                message: 'hello world',
             }),
         };
         logger.info('This is an INFO log - sending HTTP 200 - hello world response');
@@ -56,16 +40,9 @@ export const lambdaHandler = async (event: APIGatewayProxyEvent, context: Contex
         };
         logger.error('This is an ERROR log - sending HTTP 500 - some error happened response');
     } finally {
-        // Close subsegments (the AWS Lambda one is closed automatically)
-        subsegment.close(); // (### MySubSegment)
-        handlerSegment.close(); // (## index.handler)
-
-        // Set the facade segment as active again (the one created by AWS Lambda)
-        tracer.setSegment(segment);
         // Publish all stored metrics
         metrics.publishStoredMetrics();
     }
 
     return response;
-
 };
